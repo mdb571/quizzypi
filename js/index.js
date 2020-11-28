f47b582bfeb0006f1a99bce45eadd32e7b350a2d
shibam17 <shibam.dipu@gmail.com>
2020-11-28 23:41:57 +0530

    added last files

--- js/index.js
+++ js/index.js
@@ -13,28 +13,37 @@ const initial = 30;
 
 function quizQuestion (){
     fetch(`${API}`).then(response => response.json()).then(displayData);
+     quizMusic.play()
 }
 
 displayData =(res)=>{
     let quiz = res.results[0].question;
     console.log(quiz);
     quizQues.innerHTML = quiz;
-    quizMusic.play()
 
     console.log(res.results[0].correct_answer);
     answer = res.results[0].correct_answer;
 
+    startTimer();
+
 }
 
 startTimer = () =>{
-    
+    const x = setInterval(() => {
+        time.innerHTML = Number(time.innerHTML) - 1;
+    }, 1000)
+    setTimeout(() => {
+        clearInterval(x);
+        // time.innerHTML = 30;
+        // quizQuestion();
+    }, 30 * 1000)
 }
 
 window.onload = quizQuestion();
 
 
 nextBtn.addEventListener('click', quizQuestion)
-nextBtn.addEventListener('click', startTimer)
+
 
 ansBtn.addEventListener('click', ()=>{
     alert(answer)
